606d93f819a96d222dfc2882e7ef03c7bd3fd614
Nathan Hughes <nathan.h.hughes@gmail.com>
2025-04-24 15:10:25 +0000

    actually add node to install

--- spot_tools_ros/setup.py
+++ spot_tools_ros/setup.py
@@ -47,6 +47,7 @@ setup(
     entry_points={
         "console_scripts": [
             "spot_executor_node = spot_tools_ros.spot_executor_ros:main",
+            "spot_sensor_node = spot_tools_ros.spot_sensors:main",
         ],
     },
 )
