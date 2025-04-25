6408b5ca5fa5af9d44f468e6d9f421a50dd91d40
Nathan Hughes <nathan.h.hughes@gmail.com>
2025-04-25 00:34:48 +0000

    fix ros issues

--- spot_tools_ros/src/spot_tools_ros/spot_sensors.py
+++ spot_tools_ros/src/spot_tools_ros/spot_sensors.py
@@ -8,6 +8,7 @@ import tf2_ros
 
 from rclpy.node import Node
 import rclpy
+import os
 
 import bosdyn.client
 import bosdyn.client.util
@@ -21,7 +22,8 @@ SpotPixelFormat = image_pb2.Image.PixelFormat
 
 def _get_param(node, name, default):
     node.declare_parameter(name, default)
-    return node.get_parameter(name).get_parameter_value()
+    value = node.get_parameter(name).get_parameter_value()
+    return value
 
 
 def _get_local_time(robot, robot_stamp):
@@ -111,16 +113,16 @@ class CameraPublisher:
         self.name = name
         self.color_suffix = _get_param(
             node, f"{name}.color_suffix", "fisheye_image"
-        ).string_value()
+        ).string_value
         self.depth_suffix = _get_param(
             node, f"{name}.depth_suffix", "depth_in_visual_frame"
-        ).string_value()
+        ).string_value
 
-        node.declare_parameter(f"{name}.quality", 100.0)
-        self.quality = node.get_parameter(f"{name}.quality").float_value()
+        param = node.declare_parameter(f"{name}.quality", 100.0)
+        self.quality = param.get_parameter_value().double_value
 
-        node.declare_paramter(f"{name}.use_compressed", True)
-        self.use_compressed = node.get_parameter(f"{name}.use_compressed").bool_value()
+        param = node.declare_parameter(f"{name}.use_compressed", True)
+        self.use_compressed = param.get_parameter_value().bool_value
 
         color_topic = f"spot/{name}/color/image_raw"
         depth_topic = f"spot/{name}/depth/image_rect"
@@ -129,7 +131,7 @@ class CameraPublisher:
 
         if self.use_compressed:
             color_topic = f"{color_topic}/compressed"
-            self._rgb_pub = node.create_publish(CompressedImage, color_topic, 10)
+            self._rgb_pub = node.create_publisher(CompressedImage, color_topic, 10)
         else:
             self._rgb_pub = node.create_publisher(Image, color_topic, 10)
 
@@ -165,10 +167,11 @@ class CameraPublisher:
             or self._depth_info_pub.get_subscription_count() > 0
         )
 
-    def _publish(self, logger, color_stamp, color, depth):
+    def _publish(self, logger, robot, color, depth):
         if not self._has_work():
             return
 
+        color_stamp = _get_local_time(robot, color.shot.acquisition_time)
         if self._last_time is not None and self._last_time == color_stamp:
             return  # skip previously published images
 
@@ -196,22 +199,29 @@ class SpotClientNode(Node):
     def __init__(self):
         """Make a camera client."""
         super().__init__("spot_client_node")
-        robot_ip = self._get_param("robot.ip", "192.168.80.3").string_value()
-        setup_logging = self._get_param("robot.setup_logging", True).bool_value()
-        should_retry = self._get_param("robot.should_retry", False).bool_value()
-        poll_period_s = self._get_param("poll_period_s", 0.05).float_value()
-        names = self._get_param("cameras", ["frontleft", "frontright"])
+        robot_ip = self._get_param("robot.ip", "192.168.80.3").string_value
+        setup_logging = self._get_param("robot.setup_logging", True).bool_value
+        should_retry = self._get_param("robot.should_retry", False).bool_value
+        should_connect = self._get_param("connect_to_robot", False).bool_value
+        poll_period_s = self._get_param("poll_period_s", 0.05).double_value
+        names = self._get_param("cameras", ["frontleft", "frontright"]).string_array_value
+
+        self._cameras = {}
         for camera in names:
             self._cameras[camera] = CameraPublisher(self, camera)
 
-        self._robot = self.connect(robot_ip, setup_logging, should_retry)
-        self._client = self._robot.ensure_client(ImageClient.default_service_name)
+        if should_connect:
+            self._robot = self._connect(robot_ip, setup_logging, should_retry)
+            self._client = self._robot.ensure_client(ImageClient.default_service_name)
+        else:
+            self._robot = None
+            self._client = None
 
         # TODO(nathan) configurable exluded frames
         self._excluded_frames = ["vision", "odom", "body"]
         self._tfs = []
-        self._tf_broadcaster = tf2_ros.StaticTransformBroadcaster()
-        self._timer = rclpy.create_time(poll_period_s, self._callback)
+        self._tf_broadcaster = tf2_ros.StaticTransformBroadcaster(self)
+        self._timer = self.create_timer(poll_period_s, self._callback)
 
     def _get_param(self, name, default):
         return _get_param(self, name, default)
@@ -228,12 +238,21 @@ class SpotClientNode(Node):
         if setup_logging:
             bosdyn.client.util.setup_logging()
 
+        env_prefix = self._get_param("env_prefix", "ADT4_BOSDYN").string_value
+        def _get_login_info():
+            pass_env = f"{env_prefix}_PASSWORD"
+            user_env = f"{env_prefix}_USERNAME"
+            if user_env not in os.environ or pass_env not in os.environ:
+                return None, None
+
+            return os.environ[user_env], os.environ[pass_env]
+
         while True:
             try:
-                bosdyn.client.util.authenticate(robot)
+                bosdyn.client.util.authenticate(robot, _get_login_info)
                 return
             except Exception as e:
-                self.get_logger().logerr(e)
+                self.get_logger().error(e)
                 if not should_retry:
                     raise e
 
@@ -257,20 +276,24 @@ class SpotClientNode(Node):
 
     def _callback(self):
         """Poll Spot for new image messages and publish."""
-        responses = self._client.get_image(self._reqs)
-        curr_time = responses[0].shot.acquisition_time
-        if self._last_time is not None and self._last_time == curr_time:
-            return  # skip previously published images
+        names = []
+        requests = []
+        for name, camera in self._cameras.items():
+            requests += camera.requests
+            names.append(name)
 
-        for resp in responses:
-            self._publish_transforms(resp.shot)
+        if self._client is not None:
+            responses = self._client.get_image(requests)
+        else:
+            print(requests)
+            responses = []
 
-        # curr_time = _local_time(self._robot, color.shot.acquisition_time)
-        self._last_time = curr_time
-
-        rgb = responses[0]
-        depth = responses[1]
-        self._publisher.publish(self.get_logger(), curr_time, rgb, depth)
+        # shots = [resp.shot for resp in responses]
+        for idx, name in enumerate(names):
+            cam = self._cameras[name]
+            rgb = responses[2 * idx]
+            depth = responses[2 * idx + 1]
+            cam.publish(self.get_logger(), self._robot, rgb, depth)
 
 
 def main(args=None):
