38f61e252cbf22574a16bc838189f1423d42300e
Yasmin Veys <47190592+y-veys@users.noreply.github.com>
2025-08-06 16:48:34 -0400

    Feature/stop spot (#19)
    * Tell spot to stop when it gets close to an object
    
    * Added example code to test the arm movement
    
    ---------
    
    Co-authored-by: robot computer <aerospacecontrolslabmit@gmail.com>

--- spot_tools/examples/test_spot.py
+++ spot_tools/examples/test_spot.py
@@ -437,15 +437,25 @@ if __name__ == "__main__":
     # spot.take_lease()
     spot.robot.power_on(timeout_sec=20)
     spot.robot.time_sync.wait_for_sync()
-    # spot.stand()
+    spot.stand()
     # stow_arm(spot)
+
+    input("Make sure it's in a good position")
+    pose = math_helpers.SE3Pose(
+        x=0.2,
+        y=0,
+        z=0,
+        rot=math_helpers.Quat.from_pitch(0) * math_helpers.Quat.from_roll(0),
+    )
+
+    move_hand_to_relative_pose(spot, pose)
 
     # _run_open_door_test(spot, yoloworld_model_path)
     # _run_walking_test(spot)
     # _run_gaze_test(spot)
     # _run_place_test(spot)
     # _run_traj_test(spot)
-    _run_grasp_test(spot)
+    # _run_grasp_test(spot)
     # _run_segment_test(spot)
     # _run_YOLOWorld_test(spot)
 
