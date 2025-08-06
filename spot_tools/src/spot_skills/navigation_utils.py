d67b1f2b6073f418b41833637df683cc1c12577c
yveys <yveys@mit.edu>
2025-08-05 20:28:14 -0400

    Added example code to test the arm movement

--- spot_tools/src/spot_skills/navigation_utils.py
+++ spot_tools/src/spot_skills/navigation_utils.py
@@ -167,7 +167,11 @@ def follow_trajectory_continuous(
         )
         if distance_from_end < goal_tolerance:
             feedback.print("INFO", "Spot reached end of path")
-            endpoint = math_helpers.SE2Pose(x=tform_body_in_vision[0], y=tform_body_in_vision[1], angle=tform_body_in_vision[2])
+            endpoint = math_helpers.SE2Pose(
+                x=tform_body_in_vision[0],
+                y=tform_body_in_vision[1],
+                angle=tform_body_in_vision[2],
+            )
             navigate_to_absolute_pose(spot, endpoint, frame_name, stairs=stairs)
             break
 
